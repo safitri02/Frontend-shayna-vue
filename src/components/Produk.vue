f10fa8138399c809f82728b32491ab344bde6c53
unknown <safitri1337@gmail.com>
2020-10-08 20:17:43 +0700

    Integrasi product api

--- src/components/Produk.vue
+++ src/components/Produk.vue
@@ -4,11 +4,11 @@
 <section class="women-banner spad">
 <div class="container-fluid">
 <div class="row">
-<div class="col-lg-12 mt-5">
+<div class="col-lg-12 mt-5" v-if="products.length > 0">
 <carousel class="product-slider" :items="3" :autoplay="true" :nav="false">
-<div class="product-item">
+<div class="product-item" v-for="itemProduct in products" v-bind:key="itemProduct.id">
      <div class="pi-pic">
-          <img src="img/mickey1.jpg" alt="" />
+          <img v-bind:src="itemProduct.galeri[0].photo" alt="" />
           <ul>
                <li class="w-icon active">
                <a href="#"><i class="icon_bag_alt"></i></a>
@@ -17,12 +17,12 @@
           </ul>
      </div>
      <div class="pi-text">
-          <div class="catagory-name">Coat</div>
+          <div class="catagory-name">{{ itemProduct.type }}</div>
           <router-link to="/product">
-               <h5>Mickey Baggy</h5>
+               <h5>{{ itemProduct.name}}</h5>
           </router-link>
           <div class="product-price">
-               $14.00
+               $ {{ itemProduct.price }}
                <span>$35.00</span>
           </div>
      </div>
@@ -92,6 +92,9 @@
 </div>
 </carousel>
 </div>
+     <div class="col-lg-12" v-else> 
+          <p> Produk terbaru belum tersedia untuk saat ini</p>
+     </div>
 </div>
 </div>
 </section>
@@ -100,13 +103,25 @@
 </template>
 <script>
 import carousel from 'vue-owl-carousel';
+import axios from "axios";
 
 export default {
      name: "Produk",
      components: {
           carousel
+     },
+     data() {
+          return{
+               products: []
+          };
+     },
+     mounted(){
+          axios
+          .get("http://127.0.0.1:8000/api/products")
+          .them(res => (this.products = res.data.data))
+          .catsh(err => console.log(err));
      }
-}
+};
 </script>
 
 <style scoped>
